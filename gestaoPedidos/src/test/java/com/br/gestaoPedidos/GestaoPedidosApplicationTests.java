9ffe397ee73e9490ddd3d5a95ca6af256268957a
Mayara Louvain <may.louvain@gmail.com>
2025-08-18 17:52:12 -0300

    chore: Commit inicial

---
+++ gestaoPedidos/src/test/java/com/br/gestaoPedidos/GestaoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gestaoPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
